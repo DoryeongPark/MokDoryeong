35d69afbc721fe72b6c61ef1eb740e0c47aa715f
Doryeong Park <zzada0520@gmail.com>
2016-11-13 01:41:54 +0900

    Target view updated

--- app/src/main/java/com/mokdoryeong/team7/mokdoryeong/DiagnosisCreator.java
+++ app/src/main/java/com/mokdoryeong/team7/mokdoryeong/DiagnosisCreator.java
@@ -1,9 +1,16 @@
 package com.mokdoryeong.team7.mokdoryeong;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -14,7 +21,10 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import android.graphics.Rect;
 import org.opencv.core.Scalar;
+
+import java.util.ArrayList;
 
 /**
  * Created by park on 2016-10-31.
@@ -22,13 +32,20 @@ import org.opencv.core.Scalar;
 
 public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
 
+    private ArrayList<Rect> roiCandidates;
+
     private int faceX1;
     private int faceY1;
     private int faceX2;
     private int faceY2;
 
+    private FrameLayout mainLayout;
+
     private Mat imgFrame;
     private JavaCameraView javaCameraView;
+
+    private ImageView targetView = null;
+    private Rect faceDetectionArea = null;
 
     private FaceDetectionRoutine faceDetectionRoutine = null;
 
@@ -50,6 +67,21 @@ public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.C
         }
     };
 
+    private void loadTargetView(){
+        if(targetView != null){
+            mainLayout.removeView(targetView);
+        }
+
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int targetViewWidth = (int)((float)dm.heightPixels * 0.8f);
+        targetView = (ImageView)findViewById(R.id.target_view);
+        targetView.setLayoutParams(new FrameLayout.LayoutParams(targetViewWidth, targetViewWidth));
+        ViewGroup.MarginLayoutParams margin = (ViewGroup.MarginLayoutParams)targetView.getLayoutParams();
+        margin.setMargins((int)((float)(dm.widthPixels - targetViewWidth) / 2f),
+                          (int)((float)(dm.heightPixels - targetViewWidth) / 2f), 0, 0);
+        targetView.setLayoutParams(margin);
+
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,9 +89,24 @@ public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.C
 
         faceX1 = faceY1 = faceX2 = faceY2 = 0;
 
+        mainLayout = (FrameLayout)findViewById(R.id.diagnosis_mainlayout);
+
         javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(View.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
+
+        loadTargetView();
+
+        roiCandidates = new ArrayList<Rect>();
+
+    }
+
+    @Override
+    public void onWindowFocusChanged (boolean hasFocus) {
+        int[] location = new int[2];
+        targetView.getLocationInWindow(location);
+        faceDetectionArea = new Rect(location[0], location[1],
+                location[0] + targetView.getWidth(), location[1] + targetView.getHeight());
     }
 
     @Override
@@ -107,6 +154,7 @@ public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.C
             faceDetectionRoutine = new FaceDetectionRoutine(this);
             faceDetectionRoutine.start();
         }
+
     }
 
 
@@ -126,21 +174,9 @@ public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.C
         imgFrame = inputFrame.rgba();
 
 
-//        if(measureCounter == measureInterval){
-//
-//            if(faceDetectionRoutine.isRunning() == true){
-//                Mat clonedImgFrame = new Mat();
-//                imgFrame.copyTo(clonedImgFrame);
-//                faceDetectionRoutine.detectFaceROI(clonedImgFrame);
-//            }
-//
-//            measureCounter = 0;
-//        }
-
-        //Core.circle(imgFrame, new Point(faceCenterY, imgFrame.rows() - faceCenterX), 4, new Scalar(0, 255, 0), 3);
-
-        Core.rectangle(imgFrame, new Point(faceY1, imgFrame.rows() - faceX1),
-                new Point(faceY2, imgFrame.rows() - faceX2), new Scalar(0, 0, 255), 5);
+        if(faceDetectionArea.contains(faceX1, faceY1, faceX2, faceY2))
+            Core.rectangle(imgFrame, new Point(faceX1, faceY1),
+                    new Point(faceX2, faceY2), new Scalar(0, 255, 255), 4);
 
         return imgFrame;
     }
@@ -154,7 +190,19 @@ public class DiagnosisCreator extends Activity implements CameraBridgeViewBase.C
     }
 
     public void setPoints(int x1, int y1, int x2, int y2){
-        faceX1 = x1; faceY1 = y1;
-        faceX2 = x2; faceY2 = y2;
+
+        faceX1 = y1; faceY1 = imgFrame.rows() - x1;
+        faceX2 = y2; faceY2 = imgFrame.rows() - x2;
+
+        if(faceX1 == 0 && faceX2 == 0)
+            return;
+        //First condition - Is detected ROI located at appropriate face area
+        if(faceDetectionArea.contains(faceX1, faceY1, faceX2, faceY2)) {
+            if (roiCandidates.isEmpty()) {
+                roiCandidates.add(new Rect(faceX1, faceY1, faceX2, faceY2));
+                return;
+            }
+
+        }
     }
 }
